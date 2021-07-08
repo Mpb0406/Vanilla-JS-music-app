24d55d87e7360eb7ae2365fbebb60a2a9df0bdeb
Mpb0406 <79297389+Mpb0406@users.noreply.github.com>
2021-07-07 23:06:20 -0400

    First Commit

---
+++ Testing/songs.js
@@ -0,0 +1,20 @@
+export const songList = [
+  {
+    title: "I Think I'm Okay",
+    artist: "Machine Gun Kelly ft Yungblud & Travis Barker",
+    album: "Hotel Diablo",
+    url: "https://dl19.youtubetomp3music.com/file/youtubewK-8TCDrbV8320.mp3?fn=Machine%20Gun%20Kelly%20-%20I%20Think%20I%27m%20OKAY%20%5BOfficial%20Music%20Video%5D.mp3",
+  },
+  {
+    title: "Bodybag",
+    artist: "Machine Gun Kelly ft Yungblud",
+    album: "Tickets To My Downfall Deluxe Sold Out Edition",
+    url: "https://dl50.youtubetomp3music.com/file/youtube4wK8zXVoDw0320.mp3?fn=Machine%20Gun%20Kelly%20Ft.%20YUNGBLUD%20%26%20Bert%20McCracken%20of%20The%20Used%20-%20body%20bag%20(Official%20Audio).mp3",
+  },
+  {
+    title: "Cotton Candy",
+    artist: "Yungblud",
+    album: "Weird!",
+    url: "https://dl70.youtubetomp3music.com/file/youtubeyDeIAllUAWc320.mp3?fn=YUNGBLUD%20-%20cotton%20candy%20(Official%20Video).mp3",
+  },
+];
